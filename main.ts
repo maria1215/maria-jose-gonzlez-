c3d81e16a10bb5a1da9f36d4474f367a36f40d65
maria1215 <111547325+maria1215@users.noreply.github.com>
2022-08-18 20:46:15 -0500

    es muy bueno

--- main.ts
+++ main.ts
@@ -1,5 +1,48 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(200)
+        basic.showIcon(IconNames.Heart)
+    }
+    basic.clearScreen()
+})
 basic.forever(function () {
-	
+    basic.showString("Hola gente ")
+    basic.pause(500)
+    basic.clearScreen()
 })
 basic.forever(function () {
 	
